48e42dcea08c71d19086d1f67bf795a11e589aa4
Timothy Kosakoy <timothy.geole@akseleran.com>
2021-03-14 12:44:35 +0700

    Initial commit

---
+++ Movie Filter Swift/Movie Filter Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Filter Swift
+//
+//  Created by Timothy Kosakoy on 14/03/21.
+//  Copyright © 2021 Re:Utopia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Movie Filter Swift/Movie Filter SwiftTests/Movie_Filter_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  Movie_Filter_SwiftTests.swift
+//  Movie Filter SwiftTests
+//
+//  Created by Timothy Kosakoy on 14/03/21.
+//  Copyright © 2021 Re:Utopia. All rights reserved.
+//
+
+import XCTest
+@testable import Movie_Filter_Swift
+
+class Movie_Filter_SwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
